223801b2bddfc264098628364427fc657da2564a
gelvazio <gelvazio@tidas.com.br>
2023-03-29 17:51:36 -0300

    ok

--- projetos-frontend/PROJETOS FRONT-END/templates/dashboards/dashboard-principal-2/script.js
+++
@@ -1,8 +0,0 @@
-/* 
-======================================================
-
-  THE PROJECT IS OPEN-SOURCE ON GITHUB (MIT-LICENSE)
-     github.com/AbubakerSaeed/dashboard-ui-n20    
-
-======================================================
-*/
